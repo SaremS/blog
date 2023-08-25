e6a5859c9f940d256ec06705dbdb0563dda148b1
Sarem Seitz <sarem.seitz@gmail.com>
2023-08-25 07:11:36 +0200

    add kubernetes files

--- run_kubernetes.sh
+++
@@ -1,6 +0,0 @@
-cd environment
-source .env
-export KAFKA_BROKER SERVER_ADDRESS KAFKA_HOST REDDIT_CLIENT_ID REDDIT_CLIENT_SECRET KAFKA_TOPIC IMAGE_VERSION
-cd ../kubernetes
-envsubst < mono_config_template.yaml | kubectl apply -f - 
-cd ..
